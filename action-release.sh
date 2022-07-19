1cef169b6b295e00cd335a3cfe46f21b77ea3372
CI <CI@codefresh.io>
2022-07-19 14:53:33 +0300

    update

--- action-release.sh
+++ action-release.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 gh release delete latest -R saffi-codefresh/codefresh-report-image-plugins -y
 gh release create latest -R saffi-codefresh/codefresh-report-image-plugins --generate-notes
